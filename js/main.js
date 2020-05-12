9f2b1ffb736f384ff5f6164dec586e8a9f34f7f9
Maksym Poskannyi <max2000pl@gmail.com>
2020-05-12 17:42:22 +0300

    added blocks pricing and team (slick slider)

--- js/main.js
+++ js/main.js
@@ -33,4 +33,14 @@ $(document).ready(function () {
                }
      })
 
+
+     $('.multiple-items').slick({
+          infinite: true,
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          arrows: false,
+          dots: true,
+          dotsClass: 'dots-style'
+     });
 });
+
